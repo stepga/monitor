4cc5629f833081f3bebc77b15fc712bca2c0a55e
Stephan Gabert <stepga@nirgendwo.eu>
2026-06-09 13:44:22 +0200

    put nodeinfo code into dedicated package

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -4,52 +4,14 @@ import (
 	"log/slog"
 	"os"
 
-	"github.com/stepga/monitor/uname"
+	"github.com/stepga/monitor/nodeinfo"
 )
 
-type FileSystem struct {
-	MountPoint string `json:"mount_point"`
-	UsedBytes  uint64 `json:"used"`
-	TotalBytes uint64 `json:"total"`
-}
-
-type Info struct {
-	// as reported by `uname -n`
-	Hostname string `json:"hostname"`
-	// as reported by `uname -s`
-	OperatingSystemName string `json:"operating_system_name"`
-	// as reported by `uname -r`
-	OperatingSystemVersion string `json:"operating_system_version"`
-	// e.g. due to linux system upgrade and indication via
-	// /var/run/reboot-required
-	RebootRequired bool `json:"reboot_required"`
-	// an array of the mounted filesystems and their respective
-	// used and total sizes in bytes
-	FileSystems []FileSystem `json:"filesystems"`
-}
-
-func createInfo() (*Info, error) {
-	var err error
-	info := &Info{}
-
-	info.Hostname, err = uname.Hostname()
-	if err != nil {
-		return nil, err
-	}
-	info.OperatingSystemName = uname.OperatingSystemName()
-	info.OperatingSystemVersion, err = uname.OperatingSystemVersion()
-	if err != nil {
-		return nil, err
-	}
-
-	return info, nil
-}
-
 func main() {
-	info, err := createInfo()
+	info, err := nodeinfo.CreateInfo()
 	if err != nil {
-		slog.Error("createInfo() failed", "info", info)
+		slog.Error("CreateInfo() failed", "info", info)
 		os.Exit(1)
 	}
-	slog.Info("createInfo() succeeded", "info", info)
+	slog.Info("CreateInfo() succeeded", "info", info)
 }
---
+++ nodeinfo/nodeinfo.go
@@ -0,0 +1,43 @@
+package nodeinfo
+
+import (
+	"github.com/stepga/monitor/uname"
+)
+
+type FileSystem struct {
+	MountPoint string `json:"mount_point"`
+	UsedBytes  uint64 `json:"used"`
+	TotalBytes uint64 `json:"total"`
+}
+
+type NodeInfo struct {
+	// as reported by `uname -n`
+	Hostname string `json:"hostname"`
+	// as reported by `uname -s`
+	OperatingSystemName string `json:"operating_system_name"`
+	// as reported by `uname -r`
+	OperatingSystemVersion string `json:"operating_system_version"`
+	// e.g. due to linux system upgrade and indication via
+	// /var/run/reboot-required
+	RebootRequired bool `json:"reboot_required"`
+	// an array of the mounted filesystems and their respective
+	// used and total sizes in bytes
+	FileSystems []FileSystem `json:"filesystems"`
+}
+
+func CreateInfo() (*NodeInfo, error) {
+	var err error
+	info := &NodeInfo{}
+
+	info.Hostname, err = uname.Hostname()
+	if err != nil {
+		return nil, err
+	}
+	info.OperatingSystemName = uname.OperatingSystemName()
+	info.OperatingSystemVersion, err = uname.OperatingSystemVersion()
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
