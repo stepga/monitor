7da715bccfa19b196d86e81fc2b7d906657636ea
d4ryus <d4ryus@mailbox.org>
2026-06-10 10:30:28 +0200

    Implement basic node msg parsing and storage

--- listener/listener.go
+++ listener/listener.go
@@ -1,15 +1,65 @@
 package listener
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net"
+	"time"
 )
 
-func handleConnection(conn net.Conn) {
+type NodeMsg struct {
+	Name string `json:"name"`
+}
+
+type NodeInfo struct {
+	Name     string
+	LastSeen time.Time
+}
+
+func parseNodeMsg(conn net.Conn, out chan<- NodeMsg) {
 	defer conn.Close()
 
-	fmt.Printf("Got a client: %v\n", conn)
+	const maxMsgSize = 5 * 1024 * 1024 // 5 MB
+	limited := io.LimitReader(conn, maxMsgSize+1)
+
+	data, err := io.ReadAll(limited)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if len(data) > maxMsgSize {
+		fmt.Println(errors.New("JSON payload exceeds 5 MB limit"))
+		return
+	}
+
+	var msg NodeMsg
+	if err := json.NewDecoder(bytes.NewReader(data)).Decode(&msg); err != nil {
+		fmt.Printf("Failed to decode: %s\n", err)
+		return
+	}
+	out <- msg
+}
+
+func Thing(store map[string]NodeInfo, in <-chan NodeMsg) {
+	for msg := range in {
+		info, exists := store[msg.Name]
+		if exists {
+			fmt.Printf("Client ping: %s\n", msg.Name)
+			info.LastSeen = time.Now()
+			store[msg.Name] = info
+		} else {
+			fmt.Printf("New client: %s\n", msg.Name)
+			store[msg.Name] = NodeInfo{
+				Name:     msg.Name,
+				LastSeen: time.Now(),
+			}
+		}
+		fmt.Printf("Store: %v\n", store)
+	}
 }
 
 func Start(address string) (net.Listener, error) {
@@ -18,6 +68,9 @@ func Start(address string) (net.Listener, error) {
 		return nil, fmt.Errorf("listener: %s\n", err)
 	}
 
+	// TODO: Give thing a good name
+	thingChannel := make(chan NodeMsg)
+	go Thing(make(map[string]NodeInfo), thingChannel)
 	go func() {
 		for {
 			conn, err := listener.Accept()
@@ -29,7 +82,7 @@ func Start(address string) (net.Listener, error) {
 					continue
 				}
 			}
-			go handleConnection(conn)
+			go parseNodeMsg(conn, thingChannel)
 		}
 	}()
 	return listener, nil
