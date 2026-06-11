bed23c8d34b05c150cadce7c27abf729bc317a9d
d4ryus <d4ryus@mailbox.org>
2026-06-11 11:22:58 +0200

    Remove Store, this was just some placeholder code

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -7,7 +7,6 @@ import (
 	"os/signal"
 	"slices"
 	"syscall"
-	"time"
 
 	"github.com/stepga/monitor/bus"
 	"github.com/stepga/monitor/collector"
@@ -26,45 +25,6 @@ var AvailableReporters = map[string]reporter.Reporter{
 	"stdout": &stdout.StdoutReporter{},
 }
 
-type StoreInfo struct {
-	Info     string
-	LastSeen time.Time
-	Name     string
-}
-
-/*
-Store owner has to:
-- TODO: go over store every X hour and check for missing nodes
-- TODO: publish notifications
-*/
-func Store() {
-	ch := bus.Subscribe()
-	defer bus.Unsubscribe(ch)
-	store := make(map[string]StoreInfo)
-	for m := range ch {
-		switch msg := m.(type) {
-		case node.NodeInfo:
-			if _, exists := store[msg.HostName]; exists {
-				bus.Publish(fmt.Sprintf("message from known node: %s", msg.HostName))
-			} else {
-				bus.Publish(fmt.Sprintf("message from unknown node: %s", msg.HostName))
-			}
-			store[msg.HostName] = StoreInfo{
-				Name:     msg.HostName,
-				Info:     string(msg.String()),
-				LastSeen: time.Now(),
-			}
-			fmt.Printf("Store:\n")
-			for _, v := range store {
-				fmt.Printf("%s (last seen before %s): %s", v.Name, time.Until(v.LastSeen).Round(time.Second), v.Info)
-			}
-			fmt.Printf("\n")
-		default:
-			// Not interested
-		}
-	}
-}
-
 func main() {
 	config_file := flag.String("config", "config.json", "Path to config.json file")
 	flag.Parse()
@@ -73,8 +33,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	go Store()
 
 	for name, reporter := range AvailableReporters {
 		if slices.Contains(config.Cfg.Reporter, name) {
