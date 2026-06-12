82b97e3129f1427fbd305d17eadc3c406253bd3c
Stephan Gabert <stepga@nirgendwo.eu>
2026-06-12 16:17:45 +0200

    feedlike dashboard

--- webui/assets/static/script.js
+++ webui/assets/static/script.js
@@ -1,15 +1,39 @@
-function addText(boxId, text) {
-	const box = document.getElementById(boxId);
-	const div = document.createElement('div');
-	div.className = 'text-box';
-	div.textContent = text;
-	box.prepend(div);
-}
-
 document.addEventListener("DOMContentLoaded", function(){
 	const eventSrc = new EventSource("/events");
+	const feed = document.getElementById("notifications");
+
 	eventSrc.onmessage = (event) => {
-		// TODO: detect critical messages and post them into box-critical
-		addText('box-verbose', event.data);
-	};
+		var date = new Date();
+		try {
+			const data = JSON.parse(event.data);
+			feed.prepend(
+				createNotification(
+					data['SubSystemName'],
+					data['Summary'],
+					data['Report'],
+					date.toLocaleTimeString()
+				)
+			);
+		} catch (error) {
+			console.error('Failed to parse JSON in event:', error.message);
+		}
+			};
 });
+
+function createNotification(type, title, details, timestamp) {
+    const article = document.createElement("article");
+    article.className = `notification ${type}`;
+
+    article.innerHTML = `
+        <div class="meta">
+            <span>${type.toUpperCase()}</span>
+            <time>${timestamp}</time>
+        </div>
+
+        <h3>${title}</h3>
+
+        <p>${details}</p>
+    `;
+
+    return article;
+}
