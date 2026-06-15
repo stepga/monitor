08522db2380a2faf8c0a8d426b22cf920cceb09d
d4ryus <d4ryus@mailbox.org>
2026-06-15 15:22:02 +0200

    Remove obsolete Report interfaces
    Is replaced bei Info and Important

--- bus/bus.go
+++ bus/bus.go
@@ -1,7 +1,6 @@
 package bus
 
 import (
-	"encoding/json"
 	"fmt"
 	"sync"
 	"time"
@@ -94,12 +93,6 @@ type Summary interface {
 	Summary() string
 }
 
-// TODO: Remove, replaced by Info and Important
-type Report interface {
-	Summary
-	Report() string
-}
-
 type Info interface {
 	Summary
 	_info()
@@ -112,48 +105,34 @@ type Important interface {
 
 // Bus Message interface implementations
 
-func (info CertError) Report() string {
-	return fmt.Sprintf("%s: ERROR: %s",
+func (info CertError) Summary() string {
+	return fmt.Sprintf("%s: %s",
 		info.Url,
 		info.Error,
 	)
 }
 
-func (info CertExpiresSoon) Report() string {
+func (info CertExpiresSoon) Summary() string {
 	return fmt.Sprintf(
-		"%s: EXPIRES SOON %v remaining, expires %s\n",
+		"%s: expires soon, %v remaining, expires at %s",
 		info.Url,
 		info.Remaining,
 		info.Expiry.Format(time.UnixDate),
 	)
 }
 
-func (d DiskGettingFull) Report() string {
+func (d DiskGettingFull) Summary() string {
 	return fmt.Sprintf("Disk %s on %s is getting full: %s!", d.Disk.Source, d.Hostname, d.Disk.Capacity)
 }
 
-func (d DiskFineAgain) Report() string {
+func (d DiskFineAgain) Summary() string {
 	return fmt.Sprintf("Disk %s on %s is is fine again: %s!", d.Disk.Source, d.Hostname, d.Disk.Capacity)
 }
 
-func (n NodeInfo) Report() string {
-	data, err := json.MarshalIndent(n, "", "  ")
-	if err != nil {
-		return ""
-	}
-	return string(data)
-}
-
-// Summary impl for bus messages
-
-func (d DiskGettingFull) Summary() string { return d.Report() }
-func (d DiskFineAgain) Summary() string   { return d.Report() }
-func (c CertError) Summary() string       { return c.Report() }
-func (c CertExpiresSoon) Summary() string { return c.Report() }
-func (c ConfigReloaded) Summary() string  { return "Configuration reloaded" }
-func (n NewNode) Summary() string         { return fmt.Sprintf("New Node: %s", n.Hostname) }
-func (n NodeTimeout) Summary() string     { return fmt.Sprintf("NodeTimeout: %s", n.Hostname) }
-func (n NodeInfo) Summary() string        { return fmt.Sprintf("Node message from %s", n.Hostname) }
+func (c ConfigReloaded) Summary() string { return "Configuration reloaded" }
+func (n NewNode) Summary() string        { return fmt.Sprintf("New Node: %s", n.Hostname) }
+func (n NodeTimeout) Summary() string    { return fmt.Sprintf("NodeTimeout: %s", n.Hostname) }
+func (n NodeInfo) Summary() string       { return fmt.Sprintf("Node message from %s", n.Hostname) }
 
 func (DiskGettingFull) _info() {}
 func (DiskFineAgain) _info()   {}
