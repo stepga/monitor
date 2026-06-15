8abe9e9a3c8b250716a6eb2991debb0fc852d69f
d4ryus <d4ryus@mailbox.org>
2026-06-15 14:47:42 +0200

    stdout: Make important messages red

--- subsystems/stdout.go
+++ subsystems/stdout.go
@@ -3,6 +3,7 @@ package subsystems
 import (
 	"fmt"
 	"github.com/stepga/monitor/bus"
+	"os"
 )
 
 type StdoutReporter struct{}
@@ -11,10 +12,15 @@ func (r *StdoutReporter) Init() error {
 	ch := bus.Subscribe()
 	go func() {
 		defer bus.Unsubscribe(ch)
+		_, noColor := os.LookupEnv("NO_COLOR")
 		for msg := range ch {
 			switch m := msg.(type) {
 			case bus.Important:
-				fmt.Printf("IMPORTANT: %s\n", m.Summary())
+				if noColor {
+					fmt.Printf("IMPORTANT: %s\n", m.Summary())
+				} else {
+					fmt.Printf("\033[31m%s\033[0m\n", m.Summary())
+				}
 			case bus.Info:
 				fmt.Printf("%s\n", m.Summary())
 			case string:
