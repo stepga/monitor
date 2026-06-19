30e707be2d681b9f3dff20d73491ab4de87a664e
d4ryus <d4ryus@mailbox.org>
2026-06-19 09:34:46 +0200

    redmine: Don't override In Progress or Assignee

--- subsystems/redmine.go
+++ subsystems/redmine.go
@@ -17,44 +17,98 @@ type Redmine struct {
 	token string
 }
 
-func (r *Redmine) updateIssue() {
-	var body []byte
-	var err error
-	critical := store.FetchCritical()
-	if len(critical) == 0 {
-		body, err = json.Marshal(map[string]any{
-			"issue": map[string]any{
-				"status_id":      "3", // Resolved
-				"description":    "Nothing to see here",
-				"assigned_to_id": "", // Delete assignee
-			},
-		})
-	} else {
-		var description []string
+type GetStatus struct {
+	Id int `json:"id"`
+}
 
+type GetIssue struct {
+	Status GetStatus `json:"status"`
+}
+
+type GetResponse struct {
+	Issue GetIssue `json:"issue"`
+}
+
+type PutIssue struct {
+	StatusId     int     `json:"status_id"`
+	Description  string  `json:"description"`
+	AssignedToId *string `json:"assigned_to_id,omitempty"`
+}
+
+type PutResponse struct {
+	Issue PutIssue `json:"issue"`
+}
+
+func (r *Redmine) getIssueStatus(url, issue_id string) (int, error) {
+	resp, err := http.Get(fmt.Sprintf("%s/issues/%s.json?key=%s", url, issue_id, r.token))
+	if err != nil {
+		return 0, fmt.Errorf("failed to get issue: %s", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("get returned status %s", resp.Status)
+	}
+
+	reply := &GetResponse{}
+	if err := json.NewDecoder(resp.Body).Decode(reply); err != nil {
+		return 0, fmt.Errorf("could not parse reply: %s", err)
+	}
+
+	return reply.Issue.Status.Id, nil
+}
+
+func (r *Redmine) updateIssue(url, issue_id string) {
+	critical := store.FetchCritical()
+	var reply *PutResponse
+	if len(critical) == 0 {
+		delete_assignee := ""
+		reply = &PutResponse{
+			Issue: PutIssue{
+				StatusId:     3,
+				Description:  "Nothing to see here",
+				AssignedToId: &delete_assignee,
+			},
+		}
+	} else {
+		// We want to set the issue status to "New" (1), but
+		// only if its not "In progress" (2)
+		status, err := r.getIssueStatus(url, issue_id)
+		if err != nil {
+			fmt.Printf("redmine: failed to get issue: %s\n", err)
+			return
+		}
+		fmt.Printf("Current stauts: %d\n", status)
+
+		if status != 2 {
+			// If status isn't In progress (2) update it
+			// to New
+			status = 1
+		}
+
+		var description []string
 		for _, msg := range critical {
 			description = append(description, msg.Summary())
 		}
 
-		body, err = json.Marshal(map[string]any{
-			"issue": map[string]any{
-				"status_id":   "1", // New
-				"description": strings.Join(description, "\n"),
+		reply = &PutResponse{
+			Issue: PutIssue{
+				StatusId:    status,
+				Description: strings.Join(description, "\n"),
 			},
-		})
+		}
 	}
 
+	body, err := json.Marshal(reply)
 	if err != nil {
 		fmt.Printf("redmine: json marshall failed: %s\n", err)
 		return
 	}
 
-	url := fmt.Sprintf("%s/issues/%s.json?key=%s",
-		config.Cfg.Redmine.Url,
-		config.Cfg.Redmine.IssueId,
-		r.token)
-
-	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(body))
+	req, err := http.NewRequest(
+		http.MethodPut,
+		fmt.Sprintf("%s/issues/%s.json?key=%s", url, issue_id, r.token),
+		bytes.NewBuffer(body))
 	if err != nil {
 		fmt.Printf("redmine: creating request failed: %s\n", err)
 		return
@@ -87,7 +141,7 @@ func (r *Redmine) Init() error {
 		for m := range ch {
 			switch m.(type) {
 			case bus.CriticalListChanged:
-				go r.updateIssue()
+				go r.updateIssue(config.Cfg.Redmine.Url, config.Cfg.Redmine.IssueId)
 			}
 		}
 	}()
